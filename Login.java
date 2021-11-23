0b9d2ea9e8d33a0c18276b50c3a6a92e21f6ab3c
Kosaku Ohsawa <84359192+useroosawa@users.noreply.github.com>
2021-11-23 16:38:48 +0000

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,68 @@
+package chap10;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
+		String name = request.getParameter("name");
+		String pass = request.getParameter("pass");
+
+		User   user = new User(name, pass);
+		System.out.println(pass);
+		System.out.println(user.getPass());
+		LoginLogic loginLogic = new LoginLogic();
+		boolean isLogin = loginLogic.execute(user);
+		System.out.println(pass);
+		System.out.println(user.getPass());
+		if(isLogin) {
+			HttpSession session = request.getSession();
+			session.setAttribute("loginUser", user);
+//		}else {
+//			HttpSession session = request.getSession();
+//			session.removeAttribute("loginUser");
+		}
+		System.out.println(isLogin);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/loginResult.jsp");
+		dispatcher.forward(request, response);
+
+	}
+
+}
